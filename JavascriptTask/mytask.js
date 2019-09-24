a457633e796dff3dd0d054041a36b4d072dbaac4
Krishna Kishore <krishna30kishore@gmail.com>
2019-09-24 18:35:50 +0530

    Changes for the  latest

--- JavascriptTask/mytask.js
+++ JavascriptTask/mytask.js
@@ -1,15 +1,53 @@
-function createPyramid(rows)
-{
-for(var i=0;i<rows;i++) {
-var output="";
-    for(var j=0;j<rows-i;j++) {
-        output+=" ";
+// function createPyramid(rows)
+// {
+// for(var i=0;i<rows;i++) {
+// var output="";
+//     for(var j=0;j<rows-i;j++) {
+//         output+=" ";
+//     }
+//     for(var k=0;k<=i;k++) { 
+//         myi =i+1   
+//        output += String(myi)+" ";
+//     }
+//     console.log(output);  
+// }  
+// }
+ diamond(5)  
+function diamond(number) {
+    line='';
+    //up
+    for(i=1;i<=number;i++) {
+        
+        for(j=1;j<=i;j++) {
+            
+            line+=' '+j; //left
+            
+        }
+        for(j=i;j>1;j--) {
+            
+            line+=' '+(j-1); //right
+            
+        }
+        
+        
+        line+= "\n";
     }
-    for(var k=0;k<=i;k++) { 
-        myi =i+1   
-       output += String(myi)+" ";
+    
+    //down
+    for(i=number-1;i>=1;i--) {
+        
+        for(j=1;j<=i;j++) {
+            
+            line+=' '+j; //left
+            
+        }
+        for(j=i;j>1;j--) {
+            
+            line+=' '+(j-1); //right
+            
+        }
+        line+='\n';
     }
-    console.log(output);  
-}  
-}
-createPyramid('5')  
+       console.log(line);
+        
+    }
